aa2ad9ba2eb899bbb2767118723ba6434c98a436
Artem Mikhailovič <83209415+artmih24@users.noreply.github.com>
2024-04-25 11:48:15 +0300

    replaced inputs with textareas

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function PlaceFirstInputField(inputNumber) {
     const html = String.raw;
     document.getElementById("main_div").innerHTML = html `
         <div class="input_div" id="input_div_${String(inputNumber)}">
-            <input type="text" class="input_field" id="input_field_${String(inputNumber)}">
+            <textarea rows="1" cols="1" class="input_field" id="input_field_${String(inputNumber)}">
+            </textarea>
         </div>
         <div class="separator_div" id="separator_div_${String(inputNumber)}">
         </div>
@@ -54,7 +55,8 @@ function ExecCodeIn(input, inputNumber) {
         <div class="separator_div" id="separator_div_${String(inputNumber)}">
         </div>
         <div class="input_div" id="input_div_${String(inputNumber + 1)}">
-            <input type="text" class="input_field" id="input_field_${String(inputNumber + 1)}">
+            <textarea rows="1" cols="1" class="input_field" id="input_field_${String(inputNumber + 1)}">
+            </textarea>
         </div>
         <div class="separator_div" id="separator_div_${String(inputNumber + 1)}">
         </div>
@@ -69,13 +71,23 @@ function ExecCodeIn(input, inputNumber) {
     newInput.focus();
     var counter = inputNumber + 1;
     newInput.addEventListener("keyup", function(event) {
+        var keysNotForIncreasingInputWidth = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Enter"];
         if ((event.key === "Enter") && !event.shiftKey) {
-            inputNumber = ExecCodeIn(newInput, inputNumber + 1);
+            newInput.style.width = 'calc(100% - 48px)';
+            newInput.style.overflow = 'hidden';
+            ExecCodeIn(newInput, inputNumber + 1);
+        }
+        else if (event.key === "Backspace") {
+            newInput.cols = newInput.value.length;
+        }
+        else if (!(keysNotForIncreasingInputWidth.includes(event.key))) {
+            newInput.cols = newInput.value.length;
         }
         else if (event.key === "ArrowUp") {
             if ((counter >= 1) && (counter <= (inputNumber + 1))) {
                 counter -= 1;
                 prevVal = document.getElementById(`input_p_${String(counter)}`).innerHTML;
+                newInput.cols = prevVal.length;
                 newInput.value = prevVal.replace(/\s+/g, ' ').trim();
             }
         }
@@ -83,6 +95,7 @@ function ExecCodeIn(input, inputNumber) {
             if ((counter >= 0) && (counter <= (inputNumber - 1))) {
                 counter += 1;
                 prevVal = document.getElementById(`input_p_${String(counter)}`).innerHTML;
+                newInput.cols = prevVal.length;
                 newInput.value = prevVal.replace(/\s+/g, ' ').trim();
             }
         }
@@ -96,8 +109,21 @@ var firstInput = document.getElementById(`input_field_${String(inputNumber)}`);
 firstInput.tabIndex = 1;
 firstInput.focus();
 firstInput.addEventListener("keyup", function(event) {
+    var keysNotForIncreasingInputWidth = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Enter"];
     if ((event.key === "Enter") && !event.shiftKey) {
+        firstInput.style.width = 'calc(100% - 48px)';
+        firstInput.style.overflow = 'hidden';
         ExecCodeIn(firstInput, inputNumber);
+    }
+    // else if ((event.key === "Enter") && event.shiftKey) {
+    //     firstInput.rows = firstInput.value.split(/\r\n|\r|\n/).length;
+    //     firstInput.style.height = `${24 + (firstInput.rows - 30) * 30}px`
+    // }
+    else if (event.key === "Backspace") {
+        firstInput.cols = firstInput.value.length;
+    }
+    else if (!(keysNotForIncreasingInputWidth.includes(event.key))) {
+        firstInput.cols = firstInput.value.length;
     }
 });
 
